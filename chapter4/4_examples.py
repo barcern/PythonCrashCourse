4f50212d49cf3920206435e6df5045e4a6b9eab8
barcern <bcernakova01@gmail.com>
2020-05-01 08:43:17 +0100

    Chapter 4 updates

---
+++ chapter4/4_examples.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May  1 08:34:27 2020
+
+@author: barbora
+"""
+
+magicians = ['alice', 'david', 'carolina']
+for magician in magicians:
+    print(f"{magician.title()} that was a great trick!")
+    print(f"I can't wait to see your next trick, {magician.title()}.\n")
+    
+print("Thank you everyone, that was a great magic show.")
