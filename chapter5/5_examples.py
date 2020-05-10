2d128cc9583dda87e5421b716b3e4afda052d7ea
barcern <bcernakova01@gmail.com>
2020-05-10 19:37:28 +0100

    Chapter 5 updates

--- chapter5/5_examples.py
+++ chapter5/5_examples.py
@@ -65,6 +65,35 @@ user = 'marie'
 if user not in banned_users:
     print(f"{user.title()}, you can post a response if you wish.")
 
+# Simple if statements
+print("\nSimple if statements")
+age = 19
+if age >= 18:
+    print("You are old enough to vote!")
+    print("Have you registered to vote yet?")
+    
+# if-else statements
+print("\nif-else statements")
+age = 17
+if age >= 18:
+    print("You are old enough to vote!")
+    print("Have you registered to vote yet?")
+else:
+    print("Sorry, you are too young to vote.")
+    print("Please register to vote as soon as you turn 18.")
+    
+# if-elif-else statements
+print("\nif-elif-else statements")
+age = 12
+if age < 4:
+    print("Your admission cost is $0.")
+elif age < 18:
+    print("Your admission cost is $25.")
+else:
+    print("Your admission cost is $40.")
+    
+
+
 
 
 
