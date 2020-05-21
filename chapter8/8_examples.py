af79c254608b6896a04837a3553840e65cfc0081
barcern <bcernakova01@gmail.com>
2020-05-21 21:44:51 +0100

    Chapter 8 updates

--- chapter8/8_examples.py
+++ chapter8/8_examples.py
@@ -24,3 +24,56 @@ greet_user('sarah')
 #greet_user(name)
 
 
+# Positional arguments
+print("\nPositional arguments")
+def describe_pet(animal_type, pet_name):
+    """Display information about a pet."""
+    print(f"\nI have a {animal_type}.")
+    print(f"My {animal_type}'s name is {pet_name.title()}.")
+    
+describe_pet('hamster', 'harry')
+describe_pet('dog', 'willie')
+describe_pet('harry', 'hamster')
+
+
+# Keyword arguments
+print("\nKeyword arguments")
+describe_pet(animal_type='hamster', pet_name='harry')
+describe_pet(pet_name='harry', animal_type='hamster')
+
+
+# Default values and equivalent function calls
+print("\nDefault values and equivalent function calls")
+def describe_pet(pet_name, animal_type='dog'):
+    """Display information about a pet."""
+    print(f"\nI have a {animal_type}.")
+    print(f"My {animal_type}'s name is {pet_name.title()}.")
+
+# A dog named Willie
+describe_pet('willie')
+describe_pet(pet_name='willie')
+
+# A hamster named Harry
+describe_pet('harry', 'hamster')
+describe_pet(pet_name='harry', animal_type='hamster')
+describe_pet(animal_type='hamster', pet_name='harry')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
