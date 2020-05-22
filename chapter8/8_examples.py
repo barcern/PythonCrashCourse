5b8066074cdef79304ad48efec361321a487554e
barcern <bcernakova01@gmail.com>
2020-05-22 08:56:00 +0100

    Chapter 8 updates

--- chapter8/8_examples.py
+++ chapter8/8_examples.py
@@ -59,13 +59,65 @@ describe_pet(pet_name='harry', animal_type='hamster')
 describe_pet(animal_type='hamster', pet_name='harry')
 
 
+# Returning a simple value
+print("\nUsing return statement")
+def get_formatted_name(first_name, last_name):
+    """Return a full name, neatly formatted."""
+    full_name = f"{first_name} {last_name}"
+    return full_name.title()
+
+musician = get_formatted_name('jimi', 'hendrix')
+print(musician)
 
 
+# Making an argument optional
+print("\nMaking an argument optional")
+def get_formatted_name(first_name, middle_name, last_name):
+    """Return a full name, neatly formatted."""
+    full_name = f"{first_name} {middle_name} {last_name}"
+    return full_name.title()
+
+musician = get_formatted_name('john', 'lee', 'hooker')
+print(musician)
+
+# With middle name optional
+def get_formatted_name(first_name, last_name, middle_name=''):
+    """Return a full name, neatly formatted."""
+    if middle_name:
+        full_name = f"{first_name} {middle_name} {last_name}"
+    else:
+        full_name = f"{first_name} {last_name}"
+    return full_name.title()
+
+musician = get_formatted_name('jimi', 'hendrix')
+print(musician)
+musician = get_formatted_name('john', 'hooker', 'lee')
+print(musician)
 
 
+# Returning a dictionary
+print("\nReturning a dictionary")
+def build_person(first_name, last_name):
+    """Return a dictionary of information about a person."""
+    person = {'first': first_name, 'last': last_name}
+    return person
 
+musician = build_person('jimi', 'hendrix')
+print(musician)
 
+# Extended to include age
+print("\nReturning a dictionary")
+def build_person(first_name, last_name, age=None):
+    """Return a dictionary of information about a person."""
+    person = {'first': first_name, 'last': last_name}
+    if age:
+        person['age'] = age
+    return person
 
+musician = build_person('jimi', 'hendrix', 27)
+print(musician)
+musician = build_person('jimi', 'hendrix')
+print(musician)
 
 
 
