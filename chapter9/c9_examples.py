4193c371325f139a5963581bec8d7ea2512e3def
barcern <bcernakova01@gmail.com>
2020-05-27 22:39:06 +0100

    Chapter 9 updates

--- chapter9/c9_examples.py
+++ chapter9/c9_examples.py
@@ -22,3 +22,16 @@ class Dog:
     def roll_over(self):
         """Simulate rolling over in response to a command."""
         print(f"{self.name} rolled over!")
+
+# Making an instance out of the class
+my_dog = Dog('Willie', 6)
+print(f"My dog's name is {my_dog.name}.")
+print(f"My dog is {my_dog.age} years old.")
+my_dog.sit()
+my_dog.roll_over()
+
+# Making another instance
+your_dog = Dog('Lucy', 3)
+print(f"My dog's name is {your_dog.name}.")
+print(f"My dog is {your_dog.age} years old.")
+your_dog.sit()
