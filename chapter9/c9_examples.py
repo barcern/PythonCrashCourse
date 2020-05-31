dc0d68e32d760cbf7431c46ed1adcc084b36a19d
barcern <bcernakova01@gmail.com>
2020-05-31 14:17:26 +0100

    Chapter 9 updates

--- chapter9/c9_examples.py
+++ chapter9/c9_examples.py
@@ -35,3 +35,188 @@ your_dog = Dog('Lucy', 3)
 print(f"My dog's name is {your_dog.name}.")
 print(f"My dog is {your_dog.age} years old.")
 your_dog.sit()
+
+
+# Modifying attributes of an instance
+print("\n")
+class Car:
+    """A simple attempt to represent a car."""
+    
+    def __init__(self, make, model, year):
+        """Initialise attributes to describe a car."""
+        self.make = make
+        self.model = model
+        self.year = year
+        
+    def get_descriptive_name(self):
+        """Return a neatly formatted descriptive name."""
+        long_name = f"{self.year} {self.make} {self.model}"
+        return long_name.title()
+    
+my_new_car = Car('audi','a4', 2019)
+print(my_new_car.get_descriptive_name())
+
+
+# Adding in an attribute that changes over time
+class Car:
+    """A simple attempt to represent a car."""
+    
+    def __init__(self, make, model, year):
+        """Initialise attributes to describe a car."""
+        self.make = make
+        self.model = model
+        self.year = year
+        self.odometer_reading = 0
+        
+    def get_descriptive_name(self):
+        """Return a neatly formatted descriptive name."""
+        long_name = f"{self.year} {self.make} {self.model}"
+        return long_name.title()
+    
+    def read_odometer(self):
+        """Print a statement showing the car's mileage."""
+        print(f"This car has {self.odometer_reading} miles on it.")
+
+my_new_car = Car('audi','a4', 2019)
+print(my_new_car.get_descriptive_name())
+my_new_car.read_odometer()
+
+
+# Modifying an attribute's value directly
+print("\nModifying an attribute's values directly")
+my_new_car.odometer_reading = 23
+my_new_car.read_odometer()
+
+# Modying an attribute's value through a method
+print("\nModifying an attribute's value through a method")
+class Car:
+    """A simple attempt to represent a car."""
+    
+    def __init__(self, make, model, year):
+        """Initialise attributes to describe a car."""
+        self.make = make
+        self.model = model
+        self.year = year
+        self.odometer_reading = 0
+        
+    def get_descriptive_name(self):
+        """Return a neatly formatted descriptive name."""
+        long_name = f"{self.year} {self.make} {self.model}"
+        return long_name.title()
+    
+    def read_odometer(self):
+        """Print a statement showing the car's mileage."""
+        print(f"This car has {self.odometer_reading} miles on it.")
+        
+    def update_odometer(self, mileage):
+        """Set the odometer reading to the given value."""
+        self.odometer_reading = mileage
+
+my_new_car = Car('audi','a4', 2019)
+print(my_new_car.get_descriptive_name())
+
+my_new_car.update_odometer(23)
+my_new_car.read_odometer()
+
+
+# Extension to example above
+class Car:
+    """A simple attempt to represent a car."""
+    
+    def __init__(self, make, model, year):
+        """Initialise attributes to describe a car."""
+        self.make = make
+        self.model = model
+        self.year = year
+        self.odometer_reading = 0
+        
+    def get_descriptive_name(self):
+        """Return a neatly formatted descriptive name."""
+        long_name = f"{self.year} {self.make} {self.model}"
+        return long_name.title()
+    
+    def read_odometer(self):
+        """Print a statement showing the car's mileage."""
+        print(f"This car has {self.odometer_reading} miles on it.")
+        
+    def update_odometer(self, mileage):
+        """
+        Set the odometer reading to the given value.
+        Reject the change if it attempts to roll the odometer back.
+        """
+        if mileage >= self.odometer_reading:
+            self.odometer_reading = mileage
+        else:
+            print("You can't roll back an odometer!")
+
+my_new_car = Car('audi','a4', 2019)
+print(my_new_car.get_descriptive_name())
+
+my_new_car.update_odometer(23)
+my_new_car.read_odometer()
+my_new_car.update_odometer(10)
+my_new_car.read_odometer()
+
+
+# Incrementing an attribute's value through a method
+print("\nIncrementing an attribute's value through a method")
+class Car:
+    """A simple attempt to represent a car."""
+    
+    def __init__(self, make, model, year):
+        """Initialise attributes to describe a car."""
+        self.make = make
+        self.model = model
+        self.year = year
+        self.odometer_reading = 0
+        
+    def get_descriptive_name(self):
+        """Return a neatly formatted descriptive name."""
+        long_name = f"{self.year} {self.make} {self.model}"
+        return long_name.title()
+    
+    def read_odometer(self):
+        """Print a statement showing the car's mileage."""
+        print(f"This car has {self.odometer_reading} miles on it.")
+        
+    def update_odometer(self, mileage):
+        """
+        Set the odometer reading to the given value.
+        Reject the change if it attempts to roll the odometer back.
+        """
+        if mileage >= self.odometer_reading:
+            self.odometer_reading = mileage
+        else:
+            print("You can't roll back an odometer!")
+            
+    def increment_odometer(self, miles):
+        """Add the given amount to the odometer reading."""
+        self.odometer_reading += miles
+        
+my_used_car = Car('subaru', 'outback', 2015)
+print(my_used_car.get_descriptive_name())
+
+my_used_car.update_odometer(23_500)
+my_used_car.read_odometer()
+
+my_used_car.increment_odometer(100)
+my_used_car.read_odometer()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
