4b4f4d0793c786485e73b1bf9f3506967e378ea2
barcern <bcernakova01@gmail.com>
2020-06-02 00:09:54 +0100

    Chapter 9 updates

--- chapter9/c9_examples.py
+++ chapter9/c9_examples.py
@@ -216,12 +216,115 @@ my_tesla = ElectricCar('tesla', 'model s', 2019)
 print(my_tesla.get_descriptive_name())
 
 
+# Defining attributes and methods for child class
+print("\nDefiniting attributes and methods for child class")
+class ElectricCar(Car):
+    """Represents aspects of a car, specific to electric vehicles."""
+    
+    def __init__(self, make, model, year):
+        """
+        Initialise attributes of the parent class.
+        Then initialise attributes specific to an electric car.
+        """
+        super().__init__(make, model, year)
+        self.battery_size = 75
+        
+    def describe_battery(self):
+        """Print a statement describing the battery size."""
+        print(f"This car has a {self.battery_size}-kWh battery.")
+        
+my_tesla = ElectricCar('tesla', 'model s', 2019)
+print(my_tesla.get_descriptive_name())
+my_tesla.describe_battery()
 
 
+# Overriding methods from parent class - same method name, different code
+print("\nOverriding methods from parent class")
+class ElectricCar(Car):
+    """Represents aspects of a car, specific to electric vehicles."""
+    
+    def __init__(self, make, model, year):
+        """
+        Initialise attributes of the parent class.
+        Then initialise attributes specific to an electric car.
+        """
+        super().__init__(make, model, year)
+        self.battery_size = 75
+        
+    def describe_battery(self):
+        """Print a statement describing the battery size."""
+        print(f"This car has a {self.battery_size}-kWh battery.")
+        
+    def fill_gas_tank(self):
+        """Electric cars don't have gas tanks."""
+        print("This car doesn't need a gas tank!")
 
 
+# Instances as attributes - when classes get lengthy > split into smaller 
+# classes
+print("\nInstances as attributes")
+class Battery:
+    """A simple attempt to model a battery for an electric car."""
+    
+    def __init__(self, battery_size=75):
+        """Initialise the battery's attributes."""
+        self.battery_size = battery_size
+        
+    def describe_battery(self):
+        """Print a statement describing the battery size."""
+        print(f"This car has a {self.battery_size}-kWh battery.")
+        
+class ElectricCar(Car):
+    """Represents aspects of a car, specific to electric vehicles."""
+    
+    def __init__(self, make, model, year):
+        """
+        Initialise attributes of the parent class.
+        Then initialise attributes specific to an electric car.
+        """
+        super().__init__(make, model, year)
+        self.battery = Battery()
+
+my_tesla = ElectricCar('tesla', 'model s', 2019)
+print(my_tesla.get_descriptive_name())
+my_tesla.battery.describe_battery()
 
 
+# Extending example above
+class Battery:
+    """A simple attempt to model a battery for an electric car."""
+    
+    def __init__(self, battery_size=75):
+        """Initialise the battery's attributes."""
+        self.battery_size = battery_size
+        
+    def describe_battery(self):
+        """Print a statement describing the battery size."""
+        print(f"This car has a {self.battery_size}-kWh battery.")
+        
+    def get_range(self):
+        """Print a statement about the range this battery provides."""
+        if self.battery_size == 75:
+            range = 260
+        elif self.battery_size == 100:
+            range = 315
+        print(f"This car can go about {range} miles on a full charge.")
+        
+class ElectricCar(Car):
+    """Represents aspects of a car, specific to electric vehicles."""
+    
+    def __init__(self, make, model, year):
+        """
+        Initialise attributes of the parent class.
+        Then initialise attributes specific to an electric car.
+        """
+        super().__init__(make, model, year)
+        self.battery = Battery()
+
+my_tesla = ElectricCar('tesla', 'model s', 2019)
+print(my_tesla.get_descriptive_name())
+my_tesla.battery.describe_battery()
+my_tesla.battery.get_range()
 
 
 
