13a4099d3c3501eac5767a0d757300b8c19b06a1
barcern <bcernakova01@gmail.com>
2020-06-15 09:12:29 +0100

    Chapter 10 updates

--- chapter10/c10_examples.py
+++ chapter10/c10_examples.py
@@ -11,3 +11,29 @@ with open('c10_examples_pi_digits.txt') as file_object:
     contents = file_object.read()
 print(contents)
 print(contents.rstrip())
+
+
+# Using relative file paths
+print("\nRelative file paths")
+with open('text_files/c10_examples_pi_digits.txt') as file_object:
+    cotents = file_object.read()
+print(contents)
+
+
+# Using absolute file paths
+print("\nAbsolute file paths")
+file_path = '/users/barbora/Documents/c10_examples_pi_digits.txt'
+with open(file_path) as file_object:
+    contents = file_object.read()
+print(contents)
+
+
+# Reading line by line
+print("\nReading line by line")
+file_name = 'c10_examples_pi_digits.txt'
+with open(file_name) as file_object:
+    for line in file_object:
+        print(line)
+with open(file_name) as file_object:
+    for line in file_object:
+        print(line.rstrip()) 
