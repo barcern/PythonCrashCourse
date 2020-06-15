9966d5180e75b3bcdb388aa49a98e2a45c49fbfe
barcern <bcernakova01@gmail.com>
2020-06-15 22:11:55 +0100

    Chapter 10 updates

--- chapter10/c10_examples.py
+++ chapter10/c10_examples.py
@@ -36,4 +36,80 @@ with open(file_name) as file_object:
         print(line)
 with open(file_name) as file_object:
     for line in file_object:
-        print(line.rstrip()) 
+        print(line.rstrip())
+        
+
+# Making a list of lines
+print("\nMaking a list of lines")
+file_name = 'c10_examples_pi_digits.txt'
+with open(file_name) as file_object:
+    lines = file_object.readlines()
+    
+for line in lines:
+    print(line.rstrip())
+
+
+# Working with a file's contents
+print("\nWorking with a file's contents")
+file_name = 'c10_examples_pi_digits.txt'
+with open(file_name) as file_object:
+    lines = file_object.readlines()
+ 
+# Remove whitespace on right side of string only
+pi_string = ''
+for line in lines:
+    pi_string += line.rstrip()
+    
+print(pi_string)
+print(len(pi_string))
+
+# Remove whitespace on both sides of string - doesn't make a difference
+# in spyder
+pi_string = ''
+for line in lines:
+    pi_string += line.strip()
+    
+print(pi_string)
+print(len(pi_string))
+
+
+# Extended example - using one million digits of pi
+print("\nWorking with a file's contents")
+file_name = 'c10_examples_pi_million_digits.txt'
+with open(file_name) as file_object:
+    lines = file_object.readlines()
+ 
+pi_string = ''
+for line in lines:
+    pi_string += line.strip()
+    
+print(f"{pi_string[:52]}")
+print(len(pi_string))
+
+
+# Check if birthday in the first one million digits of pi
+print("\nWorking with a file's contents")
+file_name = 'c10_examples_pi_million_digits.txt'
+with open(file_name) as file_object:
+    lines = file_object.readlines()
+ 
+pi_string = ''
+for line in lines:
+    pi_string += line.strip()
+    
+birthday = input("Enter your birthday, in the form mmddyy: ")
+if birthday in pi_string:
+    print("Your birthday appears in the first million digits of pi!")
+else:
+    print("Your birthday does not appear in the first million digits of pi.")
+
+
+
+
+
+
+
+
+
+
+
