438b94cee227f58b4222959e2206fa854e956fc0
barcern <bcernakova01@gmail.com>
2020-10-03 23:08:34 +0100

    Chapter 10 refactoring

--- chapter10/c10_examples.py
+++ chapter10/c10_examples.py
@@ -255,6 +255,7 @@ print(numbers_loaded)
 
 
 # Storing and loading users' data
+print("\Storing and loading users' data")
 import json
 
 username = input("What is your name? ")
@@ -285,24 +286,83 @@ except FileNotFoundError:
 else:
     print(f"Welcome back, {username}!")
 
+# Refactoring the above
+print("\nRefactoring")
+import json
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+def greet_user():
+    """Greet the user by name."""
+    filename = 'c10_examples_username.json'
+    try:
+        with open(filename) as f:
+            username = json.load(f)
+    except FileNotFoundError:
+        username = input("What is your name? ")
+        with open(filename, 'w') as f:
+            json.dump(username, f)
+            print(f"We'll remember you when you come back, {username}!")
+    else:
+        print(f"Welcome back, {username}!")
         
+greet_user()
 
+# Refactor further
+import json
+
+def get_stored_username():
+    """Get stored username if available."""
+    filename = 'c10_examples_username.json'
+    try:
+        with open(filename) as f:
+            username = json.load(f)
+    except FileNotFoundError:
+        return None
+    else:
+        return username
+    
+def greet_user():
+    """Greet the user by name."""
+    username = get_stored_username()
+    if username:
+        print(f"Welcome back, {username}!")
+    else:
+        username = input("What is your name? ")
+        filename = 'c10_examples_username.json'
+        with open(filename, 'w') as f:
+            json.dump(username, f)
+            print(f"We'll remember you when you come back, {username}!")
+            
+greet_user()
+
+# Refactor even further
+import json
+
+def get_stored_username():
+    """Get stored username if available."""
+    filename = 'c10_examples_username.json'
+    try:
+        with open(filename) as f:
+            username = json.load(f)
+    except FileNotFoundError:
+        return None
+    else:
+        return username
+    
+def get_new_username():
+    """Prompt for a new username."""
+    username = input("What is your name? ")
+    filename = 'c10_examples_username.json'
+    with open(filename, 'w') as f:
+        json.dump(username, f)
+    return username
+    
+def greet_user():
+    """Greet the user by name."""
+    username = get_stored_username()
+    if username:
+        print(f"Welcome back, {username}!")
+    else:
+        username = get_new_username()
+        print(f"We'll remember you when you come back, {username}!")
+            
+greet_user()
