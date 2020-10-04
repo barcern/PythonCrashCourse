9b8dc8985a10bbd1a5f772316c2ddf23a12806c6
barcern <bcernakova01@gmail.com>
2020-10-04 01:27:41 +0100

    Chapter 10 complete

--- chapter10/c10_13_verify_user.py
+++ chapter10/c10_13_verify_user.py
@@ -15,17 +15,17 @@ username.
 # Starting with remember_me.py
 import json
 
+filename = 'c10_13_verify_user.json'
+
 def get_new_username():
     """Prompt for a new username."""
     username = input("What is your name? ")
-    filename = 'c10_examples_username.json'
     with open(filename, 'w') as f:
         json.dump(username, f)
     return username
 
 def get_stored_username():
     """Get stored username if available."""
-    filename = 'c10_examples_username.json'
     try:
         with open(filename) as f:
             username = json.load(f)
@@ -34,36 +34,39 @@ def get_stored_username():
     else:
         return username
     
-def greet_user():
-    """Greet the user by name."""
-    username = get_stored_username()
-    if username:
-        # Checking whether this is the correct username 
-        flag = input(f"Is {username} your username? y/n ")
-        if (flag == 'y'):
-            print(f"Welcome back, {username}!")
-        else:
-            username = get_new_username()
-            print(f"We'll remember you when you come back, {username}!")
-    else:
-        username = get_new_username()
-        print(f"We'll remember you when you come back, {username}!")
+# def greet_user():
+#     """Greet the user by name."""
+#     username = get_stored_username()
+#     if username:
+#         # Checking whether this is the correct username 
+#         flag = input(f"Is {username} your username? y/n ")
+#         if (flag == 'y'):
+#             print(f"Welcome back, {username}!")
+#         else:
+#             username = get_new_username()
+#             print(f"We'll remember you when you come back, {username}!")
+#     else:
+#         username = get_new_username()
+#         print(f"We'll remember you when you come back, {username}!")
             
-greet_user()
+# greet_user()
 
 # Clean up greet_user()
 def check_username(username):
-    flag = input(f"Is {username} your username? y/n ")
-    if (flag == 'y'):
-        print(f"Welcome back, {username}!")
-        # Using an empty return statement exits the function
-        return
+    """Check whether a username is accessible and 
+    whether it is this user's username.
+    """
+    if username:
+        flag = input(f"Is {username} your username? y/n ")
+        if (flag == 'y'):
+            print(f"Welcome back, {username}!")
+            return "stored"
     
 def greet_user_clean():
     """Greet the user by name - neater."""
     username = get_stored_username()
-    if username:
-        check_username(username)
+    if check_username(username) == "stored":
+        return
     # If the check passes, we leave the function due to empty return statement
     # If the check fails, the following lines run hence else not needed
     username = get_new_username()
