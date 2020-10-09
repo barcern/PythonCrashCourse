14d55cc5d2e14f950f0aca6c14062fb5e3a64b8e
barcern <bcernakova01@gmail.com>
2020-10-09 23:46:34 +0100

    Chapter 11 test cases

--- chapter11/c11_examples.py
+++ chapter11/c11_examples.py
@@ -5,6 +5,9 @@ Created on Mon Oct  5 22:54:34 2020
 @author: barbora
 """
 
+import unittest
+
+
 # Use function in c11_examples_name_function
 from c11_examples_name_function import get_formatted_name
 
@@ -20,3 +23,17 @@ while True:
     formatted_name = get_formatted_name(first, last)
     print(f"\tNeatly formatted name: {formatted_name}.")
 
+
+# Test case
+from c11_examples_name_function import get_formatted_name
+
+class NamesTestCase(unittest.TestCase):
+    """Tests for 'name_function.py'."""
+    
+    def test_first_last_name(self):
+        """Do names like 'Janis Joplin' work?"""
+        formatted_name = get_formatted_name('janis', 'joplin')
+        self.assertEqual(formatted_name, 'Janis Joplin')
+        
+if __name__ == '__main__':
+    unittest.main()
