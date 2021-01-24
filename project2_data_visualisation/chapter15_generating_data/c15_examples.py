7742808e3455ac862293d40fe4be362e9f3f9015
barcern <bcernakova01@gmail.com>
2021-01-24 22:58:58 +0000

    Scatter graphs

--- project2_data_visualisation/chapter15_generating_data/c15_examples.py
+++ project2_data_visualisation/chapter15_generating_data/c15_examples.py
@@ -14,10 +14,18 @@ ax.plot(squares)
 plt.show()
 
 
-# Plot improved
+# View available styles
+#plt.style.available
+
+
+# Plot improved, x values included
+input_values = [1, 2, 3, 4, 5]
 squares = [1, 4, 9, 16, 25]
+
+# Add in a style value
+plt.style.use('Solarize_Light2')
 fig, ax = plt.subplots()
-ax.plot(squares, linewidth=3)
+ax.plot(input_values, squares, linewidth=3)
 
 # Set chart title and label axes
 ax.set_title("Square Numbers", fontsize=34)
@@ -28,4 +36,38 @@ ax.set_ylabel("Square of Value", fontsize=14)
 ax.tick_params(axis='both', labelsize=14)
 
 # Show plot
+plt.show()
+
+
+# Plotting a scatter graph
+plt.style.use('seaborn')
+fig, ax = plt.subplots()
+ax.scatter(2, 4, s=200)
+
+# Set chart title and label axes
+ax.set_title("Square Numbers", fontsize=24)
+ax.set_xlabel("Value", fontsize=14)
+ax.set_ylabel("Square of Value", fontsize=14)
+
+# Set size of tick labels
+ax.tick_params(axis='both', which='major', labelsize=14)
+
+plt.show()
+
+
+# Plotting a scatter graph with a series of points
+x_values = [1, 2, 3, 4, 5]
+y_values = [1, 4, 9, 16, 25]
+plt.style.use('seaborn')
+fig, ax = plt.subplots()
+ax.scatter(x_values, y_values, s=100)
+
+# Set chart title and label axes
+ax.set_title("Square Numbers", fontsize=24)
+ax.set_xlabel("Value", fontsize=14)
+ax.set_ylabel("Square of Value", fontsize=14)
+
+# Set size of tick labels
+ax.tick_params(axis='both', which='major', labelsize=14)
+
 plt.show()
