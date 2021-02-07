8b55775c9f0ed49444becb35b8d529620537fa54
barcern <bcernakova01@gmail.com>
2021-02-07 23:51:06 +0000

    Updates to random walk

--- project2_data_visualisation/chapter15_generating_data/c15_examples.py
+++ project2_data_visualisation/chapter15_generating_data/c15_examples.py
@@ -142,6 +142,8 @@ plt.savefig('squares_plot.png', bbox_inches='tight')
 
 
 # Random walks
+from random import choice
+
 class RandomWalk:
     """A class to generate random walks."""
     
@@ -153,3 +155,23 @@ class RandomWalk:
         self.x_values = [0]
         self.y_values = [0]
         
+    def fill_walk(self):
+        """Calculate all the points in the walk."""        
+        # Keep taking steps until the walk reaches the desired length
+        while len(self.x_values) < self.num_points:
+            # Decide which direction to go and how far to go
+            x_dir = choice([1, -1])
+            x_dist = choice([0, 1, 2, 3, 4])
+            x_step = x_dir * x_dist
+            y_dir = choice([1, -1])
+            y_dist = choice([0, 1, 2, 3, 4])
+            y_step = y_dir * y_dist
+            # Reject moves that go nowhere
+            if x_step == 0 and y_step == 0:
+                continue
+            # Calculate the new position
+            x = self.x_values[-1] + x_step
+            y = self.y_values[-1] + y_step
+            self.x_values.append(x)
+            self.y_values.append(y)
+            
